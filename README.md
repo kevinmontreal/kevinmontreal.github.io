0c201d18b39191823dd381174c496f5fae44e98c
Hansong Wang <hansongwang@outlook.com>
2020-04-22 21:43:05 -0400

    update web

--- README.md
+++ README.md
@@ -1,13 +1,7 @@
-# Move to Github Page 
+# Move personal site to Github Page 
 
 New address on GitHub PAGE: [hansongwang.github.io](http://hansongwang.github.io)
 
-
-## Next Improvement
-
-[GitHub Pages Basics](https://help.github.com/categories/github-pages-basics/)
-
-Make it simple by using Bootstrap.
 
 ## Reference:
 
---
+++ flutter_service_worker.js
@@ -0,0 +1,41 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "index.html": "fc2559cb09e18b9a60748ff9ecd58369",
+"/": "fc2559cb09e18b9a60748ff9ecd58369",
+"main.dart.js": "ca90cfa7424e63140da51275414cf9dc",
+"favicon.png": "4b309aa90ea597ce9ff99878f95d273f",
+"icons/Icon-192.png": "4b309aa90ea597ce9ff99878f95d273f",
+"icons/Icon-512.png": "4b309aa90ea597ce9ff99878f95d273f",
+"manifest.json": "e6e97a7a60723df961614f3b7c61541e",
+"assets/LICENSE": "15e3b196ed5d94f4a41d0304f6b8a680",
+"assets/images/profile.png": "e877eafbc75505dc2fa8d635a8a2cce1",
+"assets/AssetManifest.json": "5d577402c7510b3a15650e31b860414f",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
