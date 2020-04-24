8d3789a489c61658f3385206327666f6e2514279
Hansong Wang <hansongwang@outlook.com>
2020-04-23 23:56:44 -0400

    update web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,15 +3,16 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "fc2559cb09e18b9a60748ff9ecd58369",
 "/": "fc2559cb09e18b9a60748ff9ecd58369",
-"main.dart.js": "d62fcf942651dcf14b935e1a3932bbe3",
+"main.dart.js": "f497589e02f4ce79febb86ea50e36679",
 "favicon.png": "4b309aa90ea597ce9ff99878f95d273f",
 "icons/Icon-192.png": "4b309aa90ea597ce9ff99878f95d273f",
 "icons/Icon-512.png": "4b309aa90ea597ce9ff99878f95d273f",
 "manifest.json": "e6e97a7a60723df961614f3b7c61541e",
 "assets/LICENSE": "358a28d6fc3a4c21802125eef462c17b",
+"assets/images/dreamhouse_color.jpg": "35425f2bcbc2259675027d5185f7c8e5",
 "assets/images/profile.png": "e877eafbc75505dc2fa8d635a8a2cce1",
 "assets/images/dreamhouse.jpg": "7939fb9febca9070d72837f3d9480abe",
-"assets/AssetManifest.json": "927cdaf6f6e869bcc0278393824432a2",
+"assets/AssetManifest.json": "07e5fbd978fabd85eb4fe87d410e6406",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
