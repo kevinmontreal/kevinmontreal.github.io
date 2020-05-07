3d99852f5f7ccaaf8560221bba8536de4eeb084f
Hansong Wang <hansongwang@outlook.com>
2020-05-07 00:01:20 -0400

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,20 +1,25 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "fc2559cb09e18b9a60748ff9ecd58369",
-"/": "fc2559cb09e18b9a60748ff9ecd58369",
-"main.dart.js": "f497589e02f4ce79febb86ea50e36679",
-"favicon.png": "4b309aa90ea597ce9ff99878f95d273f",
-"icons/Icon-192.png": "4b309aa90ea597ce9ff99878f95d273f",
-"icons/Icon-512.png": "4b309aa90ea597ce9ff99878f95d273f",
+  "index.html": "fb1e99766a2c76789a6ba2fa25a919c0",
+"/": "fb1e99766a2c76789a6ba2fa25a919c0",
+"main.dart.js": "9c38aaeb2cc200a3ad2fbfe6921930f9",
+"favicon.png": "0c5b0bc26726f87e86ed7442ad02c76a",
+"icons/Icon-192.png": "534fa97dabc788cbfe346c364cd10af3",
+"icons/Icon-512.png": "3e986fc1c7d67ee9f10b208c171276ea",
 "manifest.json": "e6e97a7a60723df961614f3b7c61541e",
-"assets/LICENSE": "358a28d6fc3a4c21802125eef462c17b",
+"assets/LICENSE": "d1cb0ff0e1964be70b7c500e5b4b1ef0",
+"assets/images/theme.png": "cafb6ec026d0b5b5144def08fcee54b9",
+"assets/images/dreamhouse.gif": "b7f5605b1c0c65cfb8de788b5e79f5d1",
 "assets/images/dreamhouse_color.jpg": "35425f2bcbc2259675027d5185f7c8e5",
-"assets/images/profile.png": "e877eafbc75505dc2fa8d635a8a2cce1",
-"assets/images/dreamhouse.jpg": "7939fb9febca9070d72837f3d9480abe",
-"assets/AssetManifest.json": "07e5fbd978fabd85eb4fe87d410e6406",
-"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/images/logo.png": "2b4a72928b661af7330c175a8bd10717",
+"assets/images/under_construction.gif": "442f2fade2d4d49a21d19a3a5f5c4ed5",
+"assets/images/profile.png": "b4714066ff94737077807fb802d90ea2",
+"assets/AssetManifest.json": "d7bf67c267faf7fcd30254b115c78c33",
+"assets/videos/dreamhouse.mp4": "ed720331d3acba337f27d7d0b45a699b",
+"assets/FontManifest.json": "96880f5cbd12a15751331cdbdac93202",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/packages/open_iconic_flutter/assets/open-iconic.woff": "3cf97837524dd7445e9d1462e3c4afe2",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
 };
 
